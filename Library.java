9cebea2ae37f9565727586f35cc197250ec023e6
Hiba-Mohamed <hibamoha987@gmail.com>
2024-10-19 17:07:39 -0230

    Library class

---
+++ Library.java
@@ -0,0 +1,31 @@
+package JAVA_sprint_1;
+import java.util.ArrayList;
+
+public class Library {
+    private ArrayList<LibraryItem> libraryItems;
+
+    public Library(){
+        this.libraryItems = new ArrayList<LibraryItem>();
+    }
+
+    public void addNewLibraryItem(){
+
+    }
+
+    public void editExistingLibraryItem(){
+
+    }
+
+    public void deleteExistingLibraryItem(){
+
+    }
+
+    public void borrowExistingLibraryItem(){
+        
+    }
+
+    public LibraryItem returnExistingLibraryItem(){
+
+    }
+    
+}
--- LibraryItem.java
+++ LibraryItem.java
@@ -2,15 +2,31 @@ package JAVA_sprint_1;
 
 public class LibraryItem {
     private int ID;
-    private String titile;
+    private String title;
     private String ISBN;
     private String publisher;
     private int numberOfCopies;
     private Author author;
 
-    public LibraryItem(int ID, String titile,String ISBN,String publisher,int numberOfCopies,Author author){
-
+    public LibraryItem(int ID, String title,String ISBN,String publisher,int numberOfCopies,Author author){
+        this.ID = ID;
+        this.title = title;
+        this.ISBN = ISBN;
+        this.publisher = publisher;
+        this.numberOfCopies = numberOfCopies;
+        this.author = author;
     }
+
+    public void editLibraryItem(int ID, String title,String ISBN,String publisher,int numberOfCopies,Author author){
+        this.ID = ID;
+        this.title = title;
+        this.ISBN = ISBN;
+        this.publisher = publisher;
+        this.numberOfCopies = numberOfCopies;
+        this.author = author;
+    }
+
+    public void deleteLibraryItem(){}
 
 
 
