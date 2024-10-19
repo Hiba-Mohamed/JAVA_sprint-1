40d582542c09540ebbc3baa638e5271bcc4ef1a3
Hiba-Mohamed <hibamoha987@gmail.com>
2024-10-19 17:36:18 -0230

    automatic Id assignment and incrementation for Lib item

--- Book.java
+++ Book.java
@@ -1,5 +1,12 @@
 package JAVA_sprint_1;
 
-public class Book extends libraryItem {
+public class Book extends LibraryItem {
+    private String type;
+
+    public Book(Stirng type){
+        super();
+        this.type = type;
+    }
+
     
 }
--- LibraryItem.java
+++ LibraryItem.java
@@ -1,6 +1,7 @@
 package JAVA_sprint_1;
 
 public class LibraryItem {
+    private static int nextId = 0;
     private int ID;
     private String title;
     private String ISBN;
@@ -8,27 +9,28 @@ public class LibraryItem {
     private int numberOfCopies;
     private Author author;
 
-    public LibraryItem(int ID, String title,String ISBN,String publisher,int numberOfCopies,Author author){
-        this.ID = ID;
+
+    public LibraryItem(String title,String ISBN,String publisher,int numberOfCopies,Author author){
+        this.ID = nextId;
+        this.title = title;
+        this.ISBN = ISBN;
+        this.publisher = publisher;
+        this.numberOfCopies = numberOfCopies;
+        this.author = author;
+        incrementNextId();  
+      }
+
+    public static void incrementNextId(){
+        nextId++;
+    }
+
+    public void editLibraryItem(String title,String ISBN,String publisher,int numberOfCopies,Author author){
+        
         this.title = title;
         this.ISBN = ISBN;
         this.publisher = publisher;
         this.numberOfCopies = numberOfCopies;
         this.author = author;
     }
-
-    public void editLibraryItem(int ID, String title,String ISBN,String publisher,int numberOfCopies,Author author){
-        this.ID = ID;
-        this.title = title;
-        this.ISBN = ISBN;
-        this.publisher = publisher;
-        this.numberOfCopies = numberOfCopies;
-        this.author = author;
-    }
-
-    public void deleteLibraryItem(){}
-
-
-
     
 }
