e58bd5e5d45dafcd2ecb6e3bd8a39f26ed85b672
Hiba-Mohamed <hibamoha987@gmail.com>
2024-10-21 12:39:03 -0230

    Setters added, constructor adjusted

--- Book.java
+++ Book.java
@@ -5,8 +5,28 @@ public class Book extends LibraryItem {
     private boolean electronic = false;
     private boolean audio= false;
 
-    public Book(String title,String ISBN,String publisher,int numberOfCopies,Author author,String type){
+    public Book(String title,String ISBN,String publisher,int numberOfCopies,Author author){
         super(title, ISBN, publisher, numberOfCopies, author);
+    }
+    public Book(String title,String ISBN,String publisher,int numberOfCopies,Author author, String type){
+        super(title, ISBN, publisher, numberOfCopies, author);
+            switch (type.toLowerCase()) {
+                case "printed":
+                    this.printed = true;
+                    break;
+                case "electronic":
+                    this.electronic = true;
+                    break;
+                case "audio":
+                    this.audio = true;
+                    break;
+                default:
+                System.err.println("Invalid book type: Please enter a valid book type ('printed' or 'electronic' or 'audio')");
+                    break;
+            }
+    }
+
+    public void setBookType(String type){
             switch (type.toLowerCase()) {
                 case "printed":
                     this.printed = true;
@@ -37,7 +57,6 @@ public class Book extends LibraryItem {
                 else return "Not Specified";
             }
         }
-
     }
 
     @Override
