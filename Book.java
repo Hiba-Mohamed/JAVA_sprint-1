010ad5f303262a70796071a4ce7e31fc9e8f0a1f
Hiba-Mohamed <hibamoha987@gmail.com>
2024-10-21 11:24:54 -0230

    moved code to correct file

--- Book.java
+++ Book.java
@@ -1,11 +1,25 @@
 package JAVA_sprint_1;
 
 public class Book extends LibraryItem {
-    private String type;
-
+    private boolean printed = false;
+    private boolean electronic = false;
+    private boolean audio= false;
     public Book(String title,String ISBN,String publisher,int numberOfCopies,Author author,String type){
-        super(title, ISBN, publisher, numberOfCopies, author);
-        this.type = type;
+super(title, ISBN, publisher, numberOfCopies, author);
+        switch (type.toLowerCase()) {
+            case "printed":
+                this.printed = true;
+                break;
+            case "electronic":
+                this.electronic = true;
+                break;
+            case "audio":
+                this.audio = true;
+                break;
+            default:
+            System.err.println("Invalid book type: Please enter a valid book type ('printed' or 'electronic' or 'audio')");
+                break;
+        }
     }
     
 }
