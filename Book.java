86edb47951982dda583be39f839af3eb8a9c78f0
Hiba-Mohamed <hibamoha987@gmail.com>
2024-10-21 11:23:05 -0230

    book type logic added

--- Book.java
+++ Book.java
@@ -3,10 +3,9 @@ package JAVA_sprint_1;
 public class Book extends LibraryItem {
     private String type;
 
-    public Book(Stirng type){
-        super();
+    public Book(String title,String ISBN,String publisher,int numberOfCopies,Author author,String type){
+        super(title, ISBN, publisher, numberOfCopies, author);
         this.type = type;
     }
-
     
 }
