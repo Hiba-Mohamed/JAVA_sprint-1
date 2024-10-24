a02817a19a004d952f73a220d1a262e7edcf1a13
SummayaSiddiqui <summaya.siddiqui@keyin.com>
2024-10-24 10:54:44 -0230

    Reset  all the flags to false first

--- Items/Periodical.java
+++ Items/Periodical.java
@@ -35,17 +35,20 @@ public class Periodical extends LibraryItem {
     }
 
 
-    public void setPeriodicalType(String type){
-            switch (type.toLowerCase()) {
-            case "printed":
-                this.printed = true;
-                break;
-            case "electronic":
-                this.electronic = true;
-                break;
-            default:
-            System.err.println("Invalid periodical type: Please enter a valid periodical type ('printed' or 'electronic')");
-                break;
+    public void setPeriodicalType(String type) {
+        // Reset all the false first to make sure only one istrue at a time
+        this.printed = false;
+        this.electronic = false;
+        switch (type.toLowerCase()) {
+        case "printed":
+            this.printed = true;
+            break;
+        case "electronic":
+            this.electronic = true;
+            break;
+        default:
+        System.err.println("Invalid periodical type: Please enter a valid periodical type ('printed' or 'electronic')");
+            break;
         }
     }
 
