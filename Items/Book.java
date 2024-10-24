53321a3e53678b8d05370aff5e2c545c25774eef
SummayaSiddiqui <summaya.siddiqui@keyin.com>
2024-10-24 10:53:30 -0230

    Reset the flags as false first

--- Items/Book.java
+++ Items/Book.java
@@ -38,21 +38,25 @@ public class Book extends LibraryItem {
         this.setBookType(type);
     }
 
-    public void setBookType(String type){
-            switch (type.toLowerCase()) {
-                case "printed":
-                    this.printed = true;
-                    break;
-                case "electronic":
-                    this.electronic = true;
-                    break;
-                case "audio":
-                    this.audio = true;
-                    break;
-                default:
-                System.err.println("Invalid book type: Please enter a valid book type ('printed' or 'electronic' or 'audio')");
-                    break;
-            }
+    public void setBookType(String type) {
+        // Reset all the false first to make sure only one istrue at a time
+        this.printed = false;
+        this.electronic = false;
+        this.audio = false;
+        switch (type.toLowerCase()) {
+            case "printed":
+                this.printed = true;
+                break;
+            case "electronic":
+                this.electronic = true;
+                break;
+            case "audio":
+                this.audio = true;
+                break;
+            default:
+            System.err.println("Invalid book type: Please enter a valid book type ('printed' or 'electronic' or 'audio')");
+                break;
+        }
     }
 
     public String getBookType(){
